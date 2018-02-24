7e1b2c63a3fac98901c499539c6103fe6446d9ca
ShaheenHossain <rapidgrps@princegroup-bd.com>
2018-02-24 13:51:32 +0600

    Update and rename __openerp__.py to __manifest__.py

--- web_pdf_preview/__manifest__.py
+++ web_pdf_preview/__manifest__.py
@@ -1,8 +1,7 @@
 # -*- coding: utf-8 -*-
 ##############################################################################
 #    Web PDF Report Preview & Print
-#   
-#
+#    
 ##############################################################################
 
 {
@@ -10,16 +9,16 @@
     'version': '10.0',
     'category': 'Web',
     'description': """Web PDF Report Preview & Print
-    
+
     """,
     'author': 'rapidgrps@gmail.com',
     'website': 'http://eagle-erp',
     'license': 'AGPL-3',
     'depends': ['web'],
-    'data': [],
-    'auto_install': False,
-    'web_preload': True,
-    'js': ['static/src/js/web_pdf_preview.js'],
+    'data': [
+        'views/web_pdf_preview.xml',
+    ],
+
 }
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
