b121fcc93bce707c6a1ee103044c1fbfc05e5c9c
ShaheenHossain <rapidgrps@princegroup-bd.com>
2018-02-24 13:23:40 +0600

    Update __init__.py

--- web_pdf_preview/controllers/__init__.py
+++ web_pdf_preview/controllers/__init__.py
@@ -1,35 +1,31 @@
 # -*- coding: utf-8 -*-
 ##############################################################################
 #    Web PDF Report Preview & Print
-#    Copyright 2012 wangbuke <wangbuke@gmail.com>
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
 
-import openerp.addons.web.http as openerpweb
-from openerp.addons.web.controllers.main import Reports
+from odoo.addons.web.controllers.main import Reports, serialize_exception
+from odoo.addons.report.controllers.main import ReportController
 import urllib
+from odoo.http import Controller, route, request
 
-class WebPdfReports(Reports):
-    _cp_path = "/web/report/pdf"
+class WebPdfReports(ReportController):
 
-    @openerpweb.httprequest
-    def index(self, req, action, token):
-        action = urllib.unquote(action)
-        result = super(WebPdfReports, self).index(req, action, token)
+    @route(['/report/preview'], type='http', auth="user")
+    def report_download(self, data, token):
+        result = super(WebPdfReports, self).report_download(data, token)
         result.headers['Content-Disposition'] = result.headers['Content-Disposition'].replace('attachment', 'inline')
         return result
+
+
+class PreviewReports(Reports):
+
+    @route('/web/report', type='http', auth="user")
+    @serialize_exception
+    def index(self, action, token):
+        result = super(PreviewReports, self).index(action, token)
+        result.headers['Content-Disposition'] = result.headers['Content-Disposition'].replace('attachment', 'inline')
+        return result
+
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
